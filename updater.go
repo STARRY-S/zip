713a245582dc6eb7ce3e5dc4dfb9b01c287709cb
José María Martín <jmmartinluque@gmail.com>
2024-03-21 15:33:46 +0100

    fix: `existingFileIndex` should start at -1
    Since 0 is the index for the first value in the file.

--- updater.go
+++ updater.go
@@ -248,7 +248,7 @@ func (u *Updater) AppendHeaderAt(fh *FileHeader, offset int64) (io.Writer, error
 	// Offset should match existing header offsets or equal to directory offset.
 	var offsetExists bool
 
-	existingFileIndex := 0
+	existingFileIndex := -1
 	if offset < 0 {
 		// Append the file to the zip or replace the existing one
 		for i, h := range u.dir {
@@ -260,7 +260,7 @@ func (u *Updater) AppendHeaderAt(fh *FileHeader, offset int64) (io.Writer, error
 		}
 
 		// File does not exist inside the zip, so we append it
-		if existingFileIndex == 0 {
+		if existingFileIndex >= 0 {
 			offset = u.dirOffset
 		}
 	} else {
